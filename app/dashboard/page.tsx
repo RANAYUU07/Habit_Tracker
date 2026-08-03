63840e615d9ae31f1a45e4e03fb425dd59c3a90e
RANAYUU07 <shadyranayuu2024@gmail.com>
2026-08-03 17:18:24 +0530

    Landing page with links, Dashboard handles real habits via map

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function DashboardPAge() {
-  const [habits, setHabits] = useState([]);
+
+  type Habit = {
+    _id: string;
+    name: string;
+    completedDates: string[];
+    userId: string;
+  };
+
+  const [habits, setHabits] = useState<Habit[]>([]); 
+
+  // now if we write useState([]), the typescript will never know what to put in it, its like making a box with no label, hence it assumes that it will be filled 'never'.
+  // and when we do habit._id or habit.name, the typescript panics as it initially thought this was somwthing not to be filled but now we are putting an id in it and name 
+  // hence we do the 'type Habit' which tells typescript that this is how the info is about to be enterd in the div element, basically we put a label on it now 
+
   const router = useRouter();
 
   useEffect(() => {
@@ -35,7 +48,11 @@ function DashboardPAge() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <pre>{JSON.stringify(habits, null, 2)}</pre>
+      {habits.map((habit) => (
+        <div key={habit._id}>
+          <p> {habit.name} </p>
+        </div>
+      ))}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-
+      <h1> Welcome to Habit Tracker </h1>
+      <p> Track your habits, one day at a time </p>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Signup</Link>
     </div>
   );
 }
